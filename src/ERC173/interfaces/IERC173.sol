4c1b372b274e14df0ada3919c225d34f1bf170b0
wighawag <wighawag@gmail.com>
2022-10-16 06:56:33 -0500

    newOwer name

--- src/ERC173/interfaces/IERC173.sol
+++ src/ERC173/interfaces/IERC173.sol
@@ -9,7 +9,7 @@ interface IERC173 {
 	function owner() external view returns (address);
 
 	/// @notice Set the address of the new owner of the contract
-	/// @dev Set _newOwner to address(0) to renounce any ownership.
-	/// @param _newOwner The address of the new owner of the contract
-	function transferOwnership(address _newOwner) external;
+	/// @dev Set newOwner to address(0) to renounce any ownership.
+	/// @param newOwner The address of the new owner of the contract
+	function transferOwnership(address newOwner) external;
 }
