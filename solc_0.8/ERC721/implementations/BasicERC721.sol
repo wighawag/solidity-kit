262daf5415093430cde4edd25824ed118f8ebb05
wighawag <wighawag@gmail.com>
2022-10-31 20:55:39 +0000

    fix

--- solc_0.8/ERC721/implementations/BasicERC721.sol
+++ solc_0.8/ERC721/implementations/BasicERC721.sol
@@ -53,7 +53,7 @@ abstract contract BasicERC721 is IERC721, ImplementingERC721Internal {
 			revert InvalidAddress(to);
 		}
 		if (msg.sender != from) {
-			if (!(operatorEnabled && _operators[id] == msg.sender) || isApprovedForAll(from, msg.sender)) {
+			if (!(operatorEnabled && _operators[id] == msg.sender) && !isApprovedForAll(from, msg.sender)) {
 				revert NotAuthorized();
 			}
 		}
@@ -176,7 +176,7 @@ abstract contract BasicERC721 is IERC721, ImplementingERC721Internal {
 		}
 
 		if (msg.sender != from) {
-			if (!((operatorEnabled && _operators[id] == msg.sender) || isApprovedForAll(from, msg.sender))) {
+			if (!((operatorEnabled && _operators[id] == msg.sender) && !isApprovedForAll(from, msg.sender))) {
 				revert NotAuthorized();
 			}
 		}
