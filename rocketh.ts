df7cc02b4e46d496e78eb9f6d929012cc6b5fac4
wighawag <wighawag@gmail.com>
2025-09-11 07:47:01 +0100

    latest hardhat-deploy

--- rocketh.ts
+++ rocketh.ts
@@ -53,7 +53,7 @@ const extensions = {
 };
 // ------------------------------------------------------------------------------------------------
 // we re-export the artifacts, so they are easily available from the alias
-import * as artifacts from './generated/artifacts.js';
+import * as artifacts from './generated/artifacts/index.js';
 export {artifacts};
 // ------------------------------------------------------------------------------------------------
 // we create the rocketh functions we need by passing the extensions to the setup function
